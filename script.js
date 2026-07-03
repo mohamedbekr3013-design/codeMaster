b0ebd30e93f014bfdea1f741c2cb911dc4fa066f
mohamedbekr3013-design <mohamedbek.r.3013@gmail.com>
2026-07-03 03:14:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function goToBook() {
+    window.location.href = "book.html";
+}
+
+// الانتقال لصفحة التعلم
+function goToLearn() {
+    window.location.href = "learn.html";
+}
+
+// تأثير بسيط عند تحميل الصفحة
+window.onload = function () {
+    console.log("CodeMaster Website Loaded 🚀");
+};
